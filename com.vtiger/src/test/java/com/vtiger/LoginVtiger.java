b94547dee75e49736ef6da11ae439201bbf5871e
Admin <Admin@Dell-PC>
2022-03-28 17:01:52 +0530

    adding new test script of login vtiger

---
+++ com.vtiger/src/test/java/com/vtiger/LoginVtiger.java
@@ -0,0 +1,47 @@
+package com.vtiger;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginVtiger {
+	@Test
+	public void loginVtiger() throws Exception {
+	String browser	=System.getProperty("browser");
+	String url	=System.getProperty("url");
+	String username	=System.getProperty("username");
+	String password	=System.getProperty("password");
+	
+	WebDriver driver=null;
+	if(browser.equals("chrome")) {
+		WebDriverManager.chromedriver().setup();
+    driver=new ChromeDriver();
+}else if(browser.equals("firefox")) {
+	WebDriverManager.firefoxdriver().setup();
+	 driver=new FirefoxDriver();
+}
+else {
+	throw new Exception("browser is not compatible");
+}
+
+driver.manage().window().maximize();
+driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		
+		
+		driver.get("url");
+		driver.findElement(By.name("user_name")).sendKeys("username");
+		driver.findElement(By.name("user_password")).sendKeys("password");
+		driver.findElement(By.id("submitButton")).click();
+		
+	}
+
+}
+
+}
